650e5b4f5e9cbce1971f5a7cd5a4d12a40e35a8d
Prakhya Mylavaram <prakhyamylavaram@Prakhyas-MacBook-Air.local>
2023-11-07 18:42:57 -0800

    First commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
+
 const routes: Routes =[
+  { path: '', component: LoginComponent },
   {
     path: '',
     redirectTo: 'dashboard',
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,8 +9,12 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
-    { path: '/user', title: 'Mail Campaign',  icon:'pe-7s-mail', class: '' },
-    ,
+    { path: '/user', title: 'Profile',  icon:'pe-7s-smile', class: '' },
+    { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
+    { path: '/typography', title: 'Transactions',  icon:'pe-7s-news-paper', class: '' },
+    { path: '/typography', title: 'Monitoring',  icon:'pe-7s-display1', class: '' },
+    { path: '/typography', title: 'Management',  icon:'pe-7s-display2', class: '' },
+    { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
     /*{ path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
     { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
     { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
